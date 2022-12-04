cfbb594c6f7fe0e27872694e088f6b3d71136f87
Simon Holden <simon.holden@gmail.com>
2022-12-04 12:36:39 +0000

    day 4 solved

--- day4.py
+++ day4.py
@@ -18,6 +18,13 @@ class srch_rec:
         return ((self.lo2>=self.lo1) and (self.hi2<=self.hi1)) or \
               ((self.lo1>=self.lo2) and (self.hi1<=self.hi2))  
 
+    def overlaps(self):
+        # turn lo1..hi1 and lo2..hi2 into a set and look for intersection
+        set1=set(range(self.lo1, self.hi1+1))
+        set2=set(range(self.lo2, self.hi2+1))
+       # print ("**overlap:",self,set1, set2)
+        return( len(set1.intersection(set2))>0 )
+
 recs=[]
 with open("day4input.txt") as file:
      for line in file:
@@ -26,15 +33,26 @@ with open("day4input.txt") as file:
         lo2,hi2=r2.split("-",1)
         recs.append(srch_rec(lo1,hi1,lo2,hi2)) 
 
-#tst=srch_rec(53,62,8,87)
-#print(tst, tst.isContained())
+#part1 - count the number of contained items (equal=contained)
+print ("part 1: list comp:",len([1 for s in recs if s.isContained() ]))
+#part2 - just count the intersections 
+print ("part 2: list comp:",len([1 for s in recs if s.overlaps() ]))
 
+# older long-winded code
+# part 1 
 cnt=0
 for s in recs:
     if s.isContained():
         #print("Contained",s)
         cnt+=1
-print("part 1 count",cnt)
+print("part 1 count - containments",cnt)
+
+cnt=0
+print("#pairs",len(recs))
+for s in recs:
+    if s.overlaps():
+        cnt+=1
+print("part 2 count - intersections",cnt)
 
 
 
