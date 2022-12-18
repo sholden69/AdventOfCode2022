e61fe1091d013b42a03d17225a57bf8f65e84142
Simon Holden <simon.holden@gmail.com>
2022-12-18 19:21:37 +0000

    part 2

--- day11 part2.py
+++ day11 part2.py
@@ -34,6 +34,9 @@ class Monkey:
         else:
             return None
 
+    def getItems(self):
+        return self.items
+
     def getInspections(self):
         return self.inspections
 
@@ -65,6 +68,14 @@ class Monkey:
             return self.true_monkey
         else:
             return self.false_monkey
+
+    def applyThrowRule2(self,itm):
+        #returns the number of the monkey to throw to 
+        rem= itm % self.divisor 
+        if rem== 0:
+            return [self.true_monkey,0]
+        else:
+            return [self.false_monkey,rem]
 
     def __repr__(self):
         outstr='Starting items:'
@@ -103,29 +114,63 @@ def buildMonkeyList(filename)-> list:
             thisMky+=1
     return mkList
 
-mylist=buildMonkeyList('Day11TestInput.txt')    
 
-t1=time.perf_counter()
-print('*** Lets Start')
-for round in range(1,1001):
-    if round % 100 ==0:
-        t2=time.perf_counter()
-        print('round',round,'in',t2-t1,'seconds')
-        t1=t2
+def part1Run(mklist, rounds): 
+    t1=time.perf_counter()
+    print('*** Lets Start')
+    for round in range(1,rounds+1):
+        if round % 100 ==0:
+            t2=time.perf_counter()
+            print('round',round,'in',t2-t1,'seconds')
+            t1=t2
+        for mky in mylist:
+            while True:
+                itm=mky.popItem()
+                if itm is None:
+                    break
+                itm=mky.applyWorryRule(itm) // 3
+            
+                # find out which monkey to throw to             
+                mylist[mky.applyThrowRule(itm) ].pushItem(itm) 
+
+    # list out the inspection count
     for mky in mylist:
-        while True:
-            itm=mky.popItem()
-            if itm is None:
-                break
-            itm=mky.applyWorryRule(itm)
-           
-            # find out which monkey to throw to             
-            mylist[mky.applyThrowRule(itm) ].pushItem(itm) 
+        print(mky.getInspections())
+            
+    top2=sorted([mky.getInspections() for mky in mylist],reverse=True)[0:2]
+    print("Monkey Busines",top2[0]*top2[1])
 
-for mky in mylist:
-    print(mky.getInspections())
-        
-top2=sorted([mky.getInspections() for mky in mylist],reverse=True)[0:2]
-print("Monkey Busines",top2[0]*top2[1])
+
+
+mylist=buildMonkeyList('Day11Input.txt')    
+#part1Run(mylist,20)
+
+itemPaths={}
+for idx,mky in enumerate(mylist):
+    for itm in mky.getItems():
+        # first mky, remainder
+        thisValue=itm
+        nextVal=mky.applyWorryRule(itm) 
+        nextMky,firstRem=mky.applyThrowRule2(itm)
+        itemPaths[thisValue]=[[idx,firstRem]] # initialise a blank list
+        while True:
+            # follow each monkey pass until you are back at first monkey with same output
+            thisMky=nextMky
+            thisVal=nextVal
+            nextVal=mylist[thisMky].applyWorryRule(thisVal) 
+            nextMky,thisRem=mylist[thisMky].applyThrowRule2(itm)
+            # need to scan through to see whther we have thisMyy, thisRem
+            breakOut=False
+            for items in itemPaths[thisValue]:
+                if items[0]==thisMky and items[1]==thisRem:
+                    breakOut=True
+                    break
+            itemPaths[thisValue].append([thisMky,thisRem])
+            print("start item",thisValue,"monkey",thisMky,"Remainder",thisRem)
+            if breakOut:
+                break
+for items in itemPaths:
+    print(items)
+
 
 
