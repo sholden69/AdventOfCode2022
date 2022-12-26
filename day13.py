a7bb7451d884489f14a1b4fc7938c37d47373f50
Simon Holden <simon.holden@gmail.com>
2022-12-26 23:56:31 +0000

    day 13 contd

--- day13.py
+++ day13.py
@@ -5,24 +5,6 @@ def strToList(aStr):
     s1= ast.literal_eval(aStr)
     return s1
 
-def flatten(aList):
-    #takes nested list and flattens it out
-    if not aList:
-        return None
-    i=0
-    ret=[]
-    while i<len(aList):
-        if type(aList[i]) is int:
-            ret.append(aList[i])
-        else:
-            for el in flatten(aList[i]):
-                ret.append(el)
-        i+=1
-    return ret
-
-
-
-
 
 def lrComp(left,right):
     print("comp left",left,"right",right)
@@ -36,26 +18,29 @@ def lrComp(left,right):
         return False
    
     if type(left) is int and type(right) is list:
-        return lrComp(list(left),right) 
+        tstRes= lrComp(list(left),right) 
+        return tstRes
     
     if type(right) is int and type(left) is list:
         return lrComp(left,list(right))
 
-   #deal with empty lists
-    if type(left) is list and type(right) is list: #both lists
-        if len(right)==1 and len(left)==1:
-            return lrComp(left[0],right[0])
-        elif len(left)==1:
-            return True
-        elif len(right)==1:
-            return False
+   #deal with lists of lists - caters for empty as well
+    if type(left) is list and type(right) is list:
+        ll=len(left)
+        lr=len(right)
+        if ll>0 and lr>0:
+            tl=type(left[0])
+            tr=type(right[0])
+            if tr is list and tl is list: 
+                return lrComp(left[0],right[0])
+        
+
+
     
     i=0
     success=True
     while True:
         # base case of two integers
-      
-            
         # quit if we are at the end of either list
         if (i>=len(left)) or (i>=len(right)):
             if i<len(left):
@@ -79,24 +64,6 @@ def lrComp(left,right):
 
     return success
 
-def lrComp2(left,right):
-    print("lrComp2",left,right)
-    i=0
-    success=True
-    while True:
-        if i>=len(left) and i>=len(right):
-            break
-        if i>=len(left):
-            break
-        if i>=len(right):
-            success=False
-            break
-        if left[i]<=right[i]:
-            i+=1
-        else:
-            return False
-    return success
-
 
 with open("Day13TestInput.txt") as f:
     raw_pairs = f.read().split("\n\n")
@@ -104,9 +71,9 @@ pairs=[p.split("\n")for p in raw_pairs]
 
 mySum=0
 for idx,p in enumerate(pairs):
-    p0=flatten(strToList(p[0]))
-    p1=flatten(strToList(p[1]))
-    if lrComp2(p0,p1):
+    p0=strToList(p[0])
+    p1=strToList(p[1])
+    if lrComp(p0,p1):
         mySum+=idx+1
         print("pair",idx+1,"matches. Score now",mySum)
 print("matching score",mySum) 
