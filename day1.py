405503660cdb0d20e1af63d12a227e8f14f1f11c
Simon Holden <simon.holden@gmail.com>
2023-01-04 17:04:16 +0000

    tidy up day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
-
+# advent of code 2022. Day 1
+# https://adventofcode.com/2022/day/1
 
 with open("day1input.txt") as file:
     lines = [line.rstrip() for line in file]
@@ -9,11 +10,11 @@ max_elf=-1
 elf_cals=0
 elf_dict = {}
 for line in lines:
-    if line=="":
+    if line=="": #onto a new elf so check totals
        if (elf_cals>max_cals) :
             max_elf=elf
             max_cals=elf_cals
-       elf_dict[elf] = elf_cals 
+       elf_dict[elf] = elf_cals #record the calories for each elf
        elf+=1
        elf_cals=0
     else:
@@ -21,10 +22,6 @@ for line in lines:
 print('elf',max_elf,'cals:',max_cals)
 
 # part 2
-sorted_elves = sorted(elf_dict.items(), key=lambda x:x[1],reverse=True)
-#top 3 elves
-total=0
-for i in range(3):
-    print(sorted_elves[i])
-    total+=sorted_elves[i][1]
+# sort the dict on the second element, slice to top 3 and then use sum
+total=sum(x[1] for x in sorted(elf_dict.items(), key=lambda x:x[1],reverse=True)[:3]) 
 print ("top 3 elves carrying",total)
