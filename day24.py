da1a8a1f55a8b5939690d4892ac44eb5823f6c9b
Simon Holden <simon.holden@gmail.com>
2023-01-04 16:38:29 +0000

    some more comments

--- day24.py
+++ day24.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env pypy3
-#Advent of Code 2022 Day 24
+#Advent of Code 2022 Day 24: https://adventofcode.com/2022/day/24
 #courtesy of: https://github.com/viliampucik/adventofcode/blob/master/2022/24.py
 def solve(start, stop, step):
-    positions = set([start])
+    #find our way from start to stop in the smallest nuber of steps
+    positions = set([start]) #initialise set with our starting point
 
     while True:
         next_positions = set()
         for r, c in positions:
+            # try moving in all directions, starting with stannding still.
             for x, y in ((r, c), (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
-                if (x, y) == stop:
+                if (x, y) == stop: # if we've made it to end then stop
                     return step
                 # fmt:off
+                # use step modulo width/height to see where the blizzards are.
+                # if this is a legitimate  move then add this to the next set of positions.
                 if 0 <= x < height and 0 <= y < width \
                    and grid[x][(y - step) % width] != ">" \
                    and grid[x][(y + step) % width] != "<" \
@@ -19,11 +23,12 @@ def solve(start, stop, step):
                     next_positions.add((x, y))
                 # fmt:on
         positions = next_positions
-        if not positions:
+        if not positions: #if nowhere to go then jump back to the start
             positions.add(start)
         step += 1
 
 
+#read in the grid, find height and width and start, stop.
 grid = [row[1:-1] for row in open("Day24Input.txt").read().splitlines()[1:-1]]
 height, width = len(grid), len(grid[0])
 start, stop = (-1, 0), (height, width - 1)
--- day25.py
+++ day25.py
@@ -3,13 +3,42 @@
 # Courtesy of https://github.com/viliampucik/adventofcode/blob/master/2022/25.py
 SNAFU = "=-012"
 
-s1, s = "", sum(
-    sum(5**i * (SNAFU.index(c) - 2) for i, c in enumerate(n[::-1]))
-    for n in open("Day25Input.txt").read().splitlines()
-)
 
-while s:
-    s, m = divmod(s + 2, 5)
-    s1 = SNAFU[m] + s1
 
-print(s1)
+def smartPart1():
+    # translate SNAFU to decimal and sum in s
+    s1, s = "", sum(
+        sum(5**i * (SNAFU.index(c) - 2) for i, c in enumerate(n[::-1]))
+        for n in open("Day25Input.txt").read().splitlines()
+    )
+
+    # reverse integer s into output string s1
+    while s:
+        #The divmod() method takes two numbers as arguments and returns their quotient and remainder in a tuple.
+        s, m = divmod(s + 2, 5)
+        s1 = SNAFU[m] + s1
+
+    print(s1)
+
+def unpickPart1():
+    s1, s = "", sum(
+        sum(5**i * (SNAFU.index(c) - 2) for i, c in enumerate(n[::-1]))
+        for n in open("Day25Input.txt").read().splitlines()
+    )
+
+    s=0
+    lines=open("Day25Input.txt").read().splitlines()
+    s=0
+    for n in lines: 
+        for i,c in enumerate(n[::-1]):
+            thisVal=5**i * (SNAFU.index(c)-2)
+            s+=thisVal
+
+    while s:
+        s, m = divmod(s + 2, 5)
+        s1 = SNAFU[m] + s1
+
+    print(s1)
+
+
+smartPart1()
